69e6e33ab88081ea6cec9f2898710b4656432ad0
Kieran Grist <kierangrist@gmail.com>
2019-10-17 10:05:08 +0100

    Week 4 - Behaviour Trees
    Week 3 - Finished implementation of desirability
    Started work on behaviour trees for week 4

--- Miner Application(Unity)/Assets/Scripts/Helper.cs
+++ Miner Application(Unity)/Assets/Scripts/Helper.cs
@@ -11,8 +11,8 @@ public class Helper
     public static float DistanceToItem(Transform Agent, Transform Target, float MaxDistance = 10)
     {
         //INVERSE LERP TO KEEP IT IN MY RANGE OF 0 TO 1
-
-        return Mathf.Clamp(Mathf.InverseLerp(0, MaxDistance, Vector3.Distance( Agent.position,Target.position)),0, 1);
+        float RET = Mathf.Clamp(Mathf.InverseLerp(0, MaxDistance, Vector3.Distance(Agent.position, Target.position)), 0, 1);
+        return RET;
     }
 }
 [System.Serializable]
@@ -21,8 +21,6 @@ public class PriorityQueue<T>
     public List<KeyValuePair<float, State<T>>> TaskQueue = new List<KeyValuePair<float, State<T>>>();
     public void Sort()
     {
-        //https://www.w3resource.com/csharp-exercises/searching-and-sorting-algorithm/searching-and-sorting-algorithm-exercise-3.php
-
-        TaskQueue = TaskQueue.OrderBy(x => x.Key ).ToList();
+       TaskQueue = TaskQueue.OrderBy(x => x.Key ).ToList();
     }
 }
