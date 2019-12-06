fa87f02824513071f742201d0b148bb5dfce6612
Kieran Grist <kierangrist@gmail.com>
2019-12-06 17:21:53 +0000

    Epsilon - Created Level 2

---
+++ Pathfinding/Assets/Scripts/NavGraph/NavGraph.cs
@@ -0,0 +1,33 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavGraph : MonoBehaviour
+{
+    //Pipeline : Create Node Tile Map ->  Generate Racetrack -> Generate Obstabcles -> Selector -> Generate Nav Mash - > Place AI units -> AI units select personality -> Place Player -> AI Unit Pathfinding -> Play!
+
+    public List<Node> Nodes = new List<Node>();
+    public static NavGraph map;
+    private void Update()
+    {
+        map = this;
+    }
+    void Awake ()
+    {
+        map = this;
+    }
+   public void GenerateNavMesh()
+    {
+        Nodes.Clear();
+        Nodes.AddRange(FindObjectsOfType<Node>());
+        for (int i = 0; i < Nodes.Count; i++)
+            Nodes[i].Reset();
+        Nodes.Clear();
+        Nodes.AddRange(FindObjectsOfType<Node>());
+        for (int i = 0; i < Nodes.Count; i++)
+        {
+            Nodes[i].Index = i;
+            Nodes[i].GenerateNeighbours();
+        }   
+    }
+}
---
+++ Pathfinding/Assets/Scripts/Selector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum RoadTypes
+{
+    Blank,
+    Straight,
+    Corner,
+    RoadtypeMax
+}
+[ExecuteInEditMode]
+public class Selector : MonoBehaviour
+{
+    public RoadTypes type = RoadTypes.Blank;
+    public RoadTypes PreviousType;
+    // Start is called before the first frame update
+
+    private void Update()
+    {
+        
+    }
+}
